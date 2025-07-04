43ce626c86a9b9a3922db1518b216cc7ee91811d
Piyush Mahato <51166024+piku-afk@users.noreply.github.com>
2025-07-04 20:57:47 +0530

    feat: initialize extension app with manifest, popup, and icons

---
+++ apps/extension/manifest.config.ts
@@ -0,0 +1,15 @@
+import { defineManifest } from '@crxjs/vite-plugin';
+
+import packageJson from '../../package.json';
+
+export default defineManifest({
+  manifest_version: 3,
+  name: 'Red Remote',
+  version: packageJson.version,
+  icons: {
+    32: 'public/icons/inactive-icon-32.png',
+  },
+  action: {
+    default_popup: 'src/popup/index.html',
+  },
+});
